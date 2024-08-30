742026983d646ea84603543869ec96ede0d6fe26
Sam Berman <tuq24841@temple.edu>
2024-08-30 14:39:17 -0400

    added random header to print_random.c

--- print_random.c
+++ print_random.c
@@ -1,29 +1,20 @@
-//Given Project code
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 #include "random.h"
 
-char* rand_string_alloc(size_t size);
-char* rand_string(char* alloc, size_t size);
 
-char* rand_string(char* s, size_t size){
-	for (int i = 0; i < size; i++) {	
-		*s = randchar();
-		s++;
-	}
-}
+int main()
+{
+	int a;
 
-char* rand_string_alloc(size_t size){
-	char *s = malloc(size + 1);
-	if (s) {
-	       	rand_string(s, size);
-	}
-       	return s;
-}
+	/* seed the randomizer */
+	srand( (unsigned)time(NULL) );
 
-void main (){
-	srand((unsigned) time (NULL));
-	char* myString = rand_string_alloc(7);
-	printf("%s", myString);
+	printf("Today's random word: ");
+	for(a=0;a<7;a++)
+		putchar( randchar() );
+	putchar('\n');
+
+	return(0);
 }
